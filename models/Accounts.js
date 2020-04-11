4be72d34df049d7ba4deecad1afe9a5414468689
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-11 17:43:55 -0400

    Created Plaid Item and Account database schemas

---
+++ models/Accounts.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const AccountSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId, 
+        required: true
+    },
+    itemId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    accountId: {
+        type: String,
+        required: true
+    },
+    mask: String,
+    balances: {
+        available: Number,
+        current: Number,
+        limit: Number,
+        isoCurrencyCode: String,
+        unofficialCurrencyCode: String
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    officialName: String,
+    subtype: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Accounts', AccountSchema)
--- models/Items.js
+++ models/Items.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId, 
+        required: true
+    },
+    accessToken: {
+        type: String,
+        required: true
+    },
+    itemId: String,
+    availableProducts: Array,
+    billedProducts: Array,
+    institutionId: String,
+    webhook: String
+})
+
+module.exports = mongoose.model('Items', ItemSchema)
