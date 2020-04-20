5dae297a11adae4377ad6cc97d6843c01c6d993f
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-19 23:36:14 -0400

    Created dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from 'Item'
+import Chart from 'Chart'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Item />
+            <Chart />
+        </div>
+    )
+}
