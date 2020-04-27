a3e8758405f0be241cbd719ca413be4b94794205
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-26 20:11:04 -0400

    Created validation schema for registration

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const joi = require('@hapi/joi')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 const Users = require('../models/Users')
 
+const registerSchema = joi.object({
+    firstName: joi.string()
+        .min()
+        .required(),
+    lastName: joi.string()
+        .min(2)
+        .required(),
+    email: joi.string()
+        .min(6)
+        .email()
+        .required(),
+    password: joi.string()
+        .min(8)
+        .required()
+})
+
 router.post('/register', async (req, res) => {
-    const { firstName, lastName, email, password } = req.body
-    console.log('test')
+
+    
 })
 
 module.exports = router;
