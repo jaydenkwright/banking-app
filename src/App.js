7e55ba41eaec4c0a8cb666573a626beeacbc1f7b
jaydenkwright <jaydenkwright3@gmail.com>
2020-05-28 19:54:07 -0400

    Created getCurrentUser() function which sets isLoggedIn to true or false based on whether access is permitted or denied

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link, 
+  Redirect
 } from "react-router-dom";
 import AccountLink from './components/AccountLink'
 import Sidebar from './components/Sidebar'
@@ -14,9 +15,31 @@ import TransactionDashboard from './components/TransactionDashboard'
 import Register from './components/Register'
 import Login from './components/Login'
 import Logout from './components/Logout'
+import axios from 'axios'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(undefined)
+
+  const getCurrentUser = async () => {
+    try{
+      const res = await axios.get('/auth/user', {
+        'withCredentials': true
+      })
+      console.log(res.data)
+      setIsLoggedIn(true)
+    }catch(err){
+      console.log(err.response.status)
+      setIsLoggedIn(false)
+    }
+  }
+  console.log(isLoggedIn)
+  useEffect(() => {
+    getCurrentUser()
+  }, [])
+
   return (
+    <div>
+    {isLoggedIn === true ? 
     <Router>
       <Sidebar />
       <div className="main">
@@ -34,19 +57,29 @@ function App() {
             <Route path="/link">
               <AccountLink />
             </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
             <Route path="/logout">
-              <Logout />
+              <Logout setIsLoggedIn={setIsLoggedIn}/>
             </Route>
           </Switch>
         </div>
       </div>
     </Router>
+    : isLoggedIn === false ?
+    <Router>
+      <Switch>
+      <Route path="/">
+          <Login setIsLoggedIn={setIsLoggedIn}/>
+        </Route> 
+      <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login setIsLoggedIn={setIsLoggedIn}/>
+        </Route>
+      </Switch>
+    </Router>
+    : 'loading'}
+    </div>
   );
 }
 
