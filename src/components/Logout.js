5a48f57dc666c2da5c5b266fbc3af62d0c6f4277
jaydenkwright <jaydenkwright3@gmail.com>
2020-05-28 19:46:22 -0400

    Added redirect to Logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from './styles/Logout.module.css'
 import axios from 'axios'
+import { useHistory } from 'react-router-dom';
 
-export default function Logout() {
-
+export default function Logout({ setIsLoggedIn }) {
+    const history = useHistory()
     const logout = async () => {
         const res = await axios.post('/auth/logout',  {
             'withCredentials': true
         })
+        setIsLoggedIn(false)
+        history.push(`/`)
     }
     return (
         <div>
