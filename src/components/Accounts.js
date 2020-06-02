6ab1443ebd8279e38e2d8e2d64e339be6c72a0f0
jaydenkwright <jaydenkwright3@gmail.com>
2020-06-01 23:40:29 -0400

    Accounts associated with current user are now displayed on the dashboard

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,21 +1,47 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
 import styles from './styles/Accounts.module.css'
+import axios from 'axios'
+import { capitalizeFirstLetter } from './functions'
 
 export default function Accounts() {
+    const [accounts, setAccounts] = useState([])
+    const [loaded, setLoaded] = useState(false)
+
+    const getAccounts = async () => {
+        try{
+            const res = await axios.get('/plaid/accounts', {
+                'withCredentials': true
+            })
+            setAccounts(res.data.accounts)
+            setLoaded(true)
+        }catch(err){
+            
+        }
+    }
+
+    useEffect(() => {
+        getAccounts()
+    }, [])
 
     return (
         <div>
-            <div className={styles.account}>
-                <div className={styles.accountTitle}>
-                    Plaid Savings
+            {loaded ? 
+                <div>
+                    {
+                        accounts.map(({name, officialName, accountId, type}, i) => 
+                            <div key={i} className={styles.account}>
+                                <div className={styles.accountTitle}>
+                                    <Link className={styles.link} to={`/account/${accountId}`}>{name}</Link>
+                                </div>
+                                <div className={styles.accountSubtitle}>
+                                    {officialName ? officialName : capitalizeFirstLetter(type)}
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
-                <div className={styles.accountSubtitle}>
-                    Plaid Silver Standard 0.1% Interest Saving
-                </div>
-            </div>
-            <div className={styles.account}>
-
-            </div>
+            : null}
         </div>
     )
 }
--- src/components/functions.js
+++ src/components/functions.js
@@ -5,3 +5,7 @@ export const displayNumber = (number) => {
 export const shortenString = (string, max) => {
     return string.length >= max ? `${string.substr(0, max)}...` : string
 }
+
+export const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
