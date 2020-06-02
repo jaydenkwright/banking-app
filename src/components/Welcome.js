37ed85ce73196869c50aab56bf577a4b66fa23ca
jaydenkwright <jaydenkwright3@gmail.com>
2020-06-02 18:42:53 -0400

    Welcome component now displays the current user's first name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link } from "react-router-dom";
 import styles from './styles/Welcome.module.css'
+import axios from 'axios'
 
 export default function Welcome() {
+    const [currentUser, setCurrentUser] = useState(undefined)
+    const [loaded, setLoaded] = useState(false)
+    const [error, setError] = useState(undefined)
+    const getCurrentUser = async () => {
+        try{
+          const res = await axios.get('/auth/user', {
+            'withCredentials': true
+        })
+          setCurrentUser(res.data.user)
+          setLoaded(true)
+        }catch(err){
+            setError('Something went wrong')
+        }
+      }
+      useEffect(() => {
+        getCurrentUser()
+      }, [])
     return (
         <div>
-            <div className={styles.welcome}>
-                <Link className={styles.link} to="/">
-                    Welcome,
-                    <div>Andy</div>
-                </Link>
-            </div>
+            {loaded ?
+                !error ?
+                <div className={styles.welcome}>
+                    <Link className={styles.link} to="/">
+                        Welcome,
+                        <div>{currentUser.firstName}</div>
+                    </Link>
+                </div>
+                : error
+            : null }
         </div>
     )
 }
