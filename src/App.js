8f7b3a3bf77cb750f4f5fbd5deb9910022a84988
jaydenkwright <jaydenkwright3@gmail.com>
2020-06-08 17:32:53 -0400

    Used React lazy and Suspense to improve performance when loading in components in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, lazy, Suspense } from 'react'
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-import AccountLink from './components/AccountLink'
-import Sidebar from './components/Sidebar'
-import AccountDashboard from './components/AccountDashboard'
-import Dashboard from './components/Dashboard'
-import TransactionDashboard from './components/TransactionDashboard'
-import Register from './components/Register'
-import Login from './components/Login'
-import Logout from './components/Logout'
-import Home from './components/Home'
 import Loading from './components/Loading'
 import axios from 'axios'
+const Home = lazy(() => import('./components/Home'))
+const Logout = lazy(() => import('./components/Logout'))
+const Login = lazy(() => import('./components/Login'))
+const Register = lazy(() => import('./components/Register'))
+const AccountLink = lazy(() => import('./components/AccountLink'))
+const TransactionDashboard = lazy(() => import('./components/TransactionDashboard'))
+const AccountDashboard = lazy(() => import('./components/AccountDashboard'))
+const Sidebar = lazy(() => import('./components/Sidebar'))
+const Dashboard = lazy(() => import('./components/Dashboard'))
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(undefined)
@@ -35,48 +35,50 @@ function App() {
   }, [])
 
   return (
-    <div id='App'>
-    {isLoggedIn === true ? 
-    <Router>
-      <Sidebar />
-      <div className="main">
-        <div className="dashboards">
+    <Suspense fallback={<Loading />}>
+      <div id='App'>
+        {isLoggedIn === true ? 
+        <Router>
+          <Sidebar />
+          <div className="main">
+            <div className="dashboards">
+              <Switch>
+                <Route path="/" exact>
+                  <Dashboard/>
+                </Route>
+                <Route path="/account/:id">
+                  <AccountDashboard/>
+                </Route>
+                <Route path="/transaction/:id">
+                  <TransactionDashboard/>
+                </Route>
+                <Route path="/link">
+                  <AccountLink />
+                </Route>
+                <Route path="/logout">
+                  <Logout setIsLoggedIn={setIsLoggedIn}/>
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </Router>
+        : isLoggedIn === false ?
+        <Router>
           <Switch>
-            <Route path="/" exact>
-              <Dashboard/>
+          <Route exact path="/">
+              <Home />
+            </Route> 
+          <Route path="/register">
+              <Register />
             </Route>
-            <Route path="/account/:id">
-              <AccountDashboard/>
-            </Route>
-            <Route path="/transaction/:id">
-              <TransactionDashboard/>
-            </Route>
-            <Route path="/link">
-              <AccountLink />
-            </Route>
-            <Route path="/logout">
-              <Logout setIsLoggedIn={setIsLoggedIn}/>
+            <Route path="/login">
+              <Login setIsLoggedIn={setIsLoggedIn}/>
             </Route>
           </Switch>
-        </div>
+        </Router>
+        : <Loading />}
       </div>
-    </Router>
-    : isLoggedIn === false ?
-    <Router>
-      <Switch>
-      <Route exact path="/">
-          <Home />
-        </Route> 
-      <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login setIsLoggedIn={setIsLoggedIn}/>
-        </Route>
-      </Switch>
-    </Router>
-    : <Loading />}
-    </div>
+    </Suspense>
   );
 }
 
